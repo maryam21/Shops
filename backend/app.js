c907dd54c6b6c4a0192787c89bba2ce2452af68a
Mariam Ahhttouche <mariam2ac1@gmail.com>
2018-06-29 18:25:31 +0000

    Fix eslint errors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -21,15 +20,13 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // output the error to the console
+  // set the response status to the error status
   res.status(err.status || 500);
-  console.log(err.status+' '+err.message);
-  console.log(err.stack);
 });
 
 module.exports = app;
