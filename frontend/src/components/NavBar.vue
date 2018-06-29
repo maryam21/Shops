1a4516908a104480a7061b55704fbed2b5f0b338
Mariam Ahhttouche <mariam2ac1@gmail.com>
2018-06-29 08:47:42 +0000

    Add styling to navbar component

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -4,3 +4,19 @@
     <a href='#'> My preferred Shops </a>
   </div>
 </template>
+
+<style scoped>
+  div {
+    position: fixed;
+    top: 0;
+    right: 0;
+    padding: 20px;
+  }
+  a {
+    padding: 10px;
+    text-decoration: none;
+  }
+  a:active {
+    text-decoration: underline;
+  }
+</style>
