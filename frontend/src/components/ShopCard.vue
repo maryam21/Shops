2c5c76dc44d5c49c7106e6a5c91b88ecca950055
Mariam Ahhttouche <mariam2ac1@gmail.com>
2018-06-29 11:30:00 +0000

    Add bootstrap card in shop card component template

--- frontend/src/components/ShopCard.vue
+++ frontend/src/components/ShopCard.vue
@@ -1,9 +1,11 @@
 <template>
-    <div>
-        
-    </div>
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-danger">Dislike</a>
+            <a href="#" class="btn btn-success">Like</a>
+        </div>
+    </div>  
 </template>
-
-<script>
-    
-</script>
