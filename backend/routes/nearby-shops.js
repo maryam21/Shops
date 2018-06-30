36791eee997a01cfd1c431082d57026e3577777a
Mariam Ahhttouche <mariam2ac1@gmail.com>
2018-06-30 23:08:22 +0000

    Do a request to google places api to fetch nearby shops

--- backend/routes/nearby-shops.js
+++ backend/routes/nearby-shops.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
+
+/** Require and configure dotenv and specify relative path to .env */
+
+require('dotenv').config({path: '../.env'});
 
 /* POST nearby shops using user current position. */
 router.post('/', function(req, res, next) {
-    res.send(
-    [{
-      title: "shops",
-      description: "all",
-      body: req.body
-    }]
-    )
+    axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${req.body[0]},${req.body[1]}&radius=1500&type=shop&keyword=shop&key=${process.env.API_KEY}`)
+    .then(response => {
+        console.log(response.data);
+    })
+    .catch(error => {
+        console.log(error);
+    });
     console.log(req.body);
 });
 
