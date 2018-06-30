ba6ab0097bae823f7a973e7b7188022847eaee18
Mariam Ahhttouche <mariam2ac1@gmail.com>
2018-06-30 18:17:35 +0000

    Configure backend to use environement variables

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 
+/** Require and configure dotenv and specify relative path to .env */
+
+require('dotenv').config({path: '../.env'});
+
 var indexRouter = require('./routes/index');
 var shopsRouter = require('./routes/nearby-shops');
 
@@ -13,7 +17,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_HOST
+}));
 
 app.use('/', indexRouter);
 app.use('/nearby-shops', shopsRouter);
