2e7ac744f1c2f0aaccc5b61a25d96667c7c6ad62
Mariam Ahhttouche <mariam2ac1@gmail.com>
2018-07-01 11:13:47 +0000

    Send response data back to client

--- backend/routes/nearby-shops.js
+++ backend/routes/nearby-shops.js
@@ -11,6 +11,7 @@ router.post('/', function(req, res, next) {
     axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${req.body[0]},${req.body[1]}&radius=1500&type=shop&keyword=shop&key=${process.env.API_KEY}`)
     .then(response => {
         console.log(response.data);
+        res.send(response.data)
     })
     .catch(error => {
         console.log(error);
