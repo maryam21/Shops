2a1053dd823f1f5605aa14f09b44dc677f0f1c70
Mariam Ahhttouche <mariam2ac1@gmail.com>
2018-07-01 11:38:47 +0000

    Update nearby-shops route to use a get request

--- backend/routes/nearby-shops.js
+++ backend/routes/nearby-shops.js
@@ -6,17 +6,17 @@ const axios = require('axios');
 
 require('dotenv').config({path: '../.env'});
 
-/* POST nearby shops using user current position. */
-router.post('/', function(req, res, next) {
-    axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${req.body[0]},${req.body[1]}&radius=1500&type=shop&keyword=shop&key=${process.env.API_KEY}`)
+/* GET nearby shops using user current position. */
+router.get('/', function(req, res, next) {
+
+    axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${req.query.latitude},${req.query.longitude}&radius=1500&type=shop&keyword=shop&key=${process.env.API_KEY}`)
     .then(response => {
-        console.log(response.data);
+
         res.send(response.data)
     })
     .catch(error => {
         console.log(error);
     });
-    console.log(req.body);
 });
 
 module.exports = router;
