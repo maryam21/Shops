9d73e5b38d42b6d5e51abe36dcd45d2933b7f266
Mariam Ahhttouche <mariam2ac1@gmail.com>
2018-07-03 22:46:18 +0000

    Add methods to user model for generating hash and verifying password

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 /** Define the schema for our user model */
 
@@ -14,6 +15,18 @@ const UserSchema = new Schema({
     required: true,
   },
 });
+
+/** Generating a password hash */
+
+UserSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+/** Check if password is valid */
+
+UserSchema.methods.verifyPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
 
 /** Create the model for users and expose it to our app */
 
