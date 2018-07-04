76f37cc07ab3f689b99272386a77f7ca0868be37
Mariam Ahhttouche <mariam2ac1@gmail.com>
2018-07-04 09:59:40 +0000

    Remove sync methods

--- backend/models/user.js
+++ backend/models/user.js
@@ -19,13 +19,13 @@ const UserSchema = new Schema({
 /** Generating a password hash */
 
 UserSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+    return bcrypt.hash(password, bcrypt.genSalt(8), null);
 };
 
 /** Check if password is valid */
 
 UserSchema.methods.verifyPassword = function(password) {
-    return bcrypt.compareSync(password, this.password);
+    return bcrypt.compare(password, this.password);
 };
 
 /** Create the model for users and expose it to our app */
