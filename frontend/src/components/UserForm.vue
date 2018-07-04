66626e750d945af8ef4ecccae70cf6d113bad58b
Mariam Ahhttouche <mariam2ac1@gmail.com>
2018-07-04 21:57:55 +0000

    Add name attribute to form input fields

--- frontend/src/components/UserForm.vue
+++ frontend/src/components/UserForm.vue
@@ -3,11 +3,11 @@
       <form :action="action" method="post">
         <div class="form-group lign-items-center">
             <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email">
           </div>
         <div class="form-group">
             <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
         </div>
             <button type="submit" class="btn btn-primary">{{ submit }}</button>
       </form> 
