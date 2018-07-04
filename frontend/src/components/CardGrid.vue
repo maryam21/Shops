52b78ba2235fa07f3e28243c5626ce3c0949ff06
Mariam Ahhttouche <mariam2ac1@gmail.com>
2018-07-04 08:48:11 +0000

    Add new component CardGrid

---
+++ frontend/src/components/CardGrid.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row">
+      <div class="col-sm-3" v-for="shop in shops">
+        <shop-card :title="shop.name" :photos="shop.photos"></shop-card>
+      </div>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import ShopCard from '../components/ShopCard.vue';
+Vue.component('shop-card', ShopCard);
+
+export default {
+  props: {
+    shops: Array,
+  }
+}
+</script>
