1733e3c46eb54a26a9ae7ed5c186de8983d6237f
Mariam Ahhttouche <mariam2ac1@gmail.com>
2018-07-05 10:09:36 +0000

    Add login controller

---
+++ backend/routes/api/models/user/login.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+/** POST to login user */
+
+router.post('/', passport.authenticate('local-login', {
+
+        // Redirect to the main page
+        successRedirect : process.env.CLIENT_HOST,
+
+        // Redirect back to the signup page when there is an error
+        failureRedirect : process.env.CLIENT_HOST+'/login',
+
+        // Allow flash messages
+        failureFlash : true
+})
+);
+
+module.exports = router;
