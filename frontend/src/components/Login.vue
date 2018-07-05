de8c85da75748c8a49419e23efbaff2db90b9c84
Mariam Ahhttouche <mariam2ac1@gmail.com>
2018-07-05 09:53:00 +0000

    Add login vue component

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <nav-bar></nav-bar>
+      <user-form :action="action" submit="Login"></user-form>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import UserForm from '../components/UserForm.vue';
+
+Vue.component('user-form', UserForm);
+ export default {
+    data() {
+        return {
+            action: `${process.env.SERVER_API}/models/user/login`
+        };
+    }
+ };
+</script>
