fce08f2a4b070c9db0d71a19295ccb42c2d56a82
Mariam Ahhttouche <mariam2ac1@gmail.com>
2018-07-10 06:03:26 +0000

    Add photo route to do api call to google place photo api and return image url

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,7 @@ mongoose.connect(process.env.DATABASE);
 const shopsRouter = require('./routes/api/models/shop/shops');
 const signupRouter = require('./routes/api/models/user/signup');
 const loginRouter = require('./routes/api/models/user/login');
+const photoRouter = require('./routes/api/models/shop/photo');
 
 const app = express();
 
@@ -41,6 +42,7 @@ app.use(flash());
 app.use('/api/models/shop/shops', shopsRouter);
 app.use('/api/models/user/signup', signupRouter);
 app.use('/api/models/user/login', loginRouter);
+app.use('/api/models/shop/photo', photoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
